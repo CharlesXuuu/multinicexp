ebb8188c36863c7ec907746f624257ae35849c2a
wangliandehanyun <xuchi.int@gmail.com>
2016-05-05 16:46:30 -0700

    update

--- domu/mndomu.c
+++ domu/mndomu.c
@@ -160,7 +160,7 @@ static int __init init_domumodule(void)
     }
 
     //strcpy((char*)page, &readbuf);
-    memcpy((char*)page,readbuf,8);
+    memcpy((char*)page, "chix:12345");
 
     printk("\n gref = %d", info.gref);
     return 0;
